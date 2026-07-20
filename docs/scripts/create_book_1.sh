2082908c5050b4f8a4ff58ca9cd824f7e82f32b6
richelbilderbeek <rjcbilderbeek@gmail.com>
2026-07-20 11:03:35 +0200

    Booklets are bilingual

--- docs/scripts/create_book_1.sh
+++ docs/scripts/create_book_1.sh
@@ -21,25 +21,42 @@ cp book_style.theme $build_folder
 
 cd "${build_folder}" || exit 41
 
+Rscript -e "ignored_output <- splimata::split_tabs(input_file_name = \"README_00.md\", output_file_prefix = \"generated_chapter_0\")" || exit 42
+Rscript -e "ignored_output <- splimata::split_tabs(input_file_name = \"README_01.md\", output_file_prefix = \"generated_chapter_1\")" || exit 42
+Rscript -e "ignored_output <- splimata::split_tabs(input_file_name = \"README_02.md\", output_file_prefix = \"generated_chapter_2\")" || exit 42
+
+
 { \
-  cat README_00.md ; echo " " ; echo "\pagebreak" ; echo " "; \
-  cat README_01.md ; echo " " ; echo "\pagebreak" ; echo " "; \
-  cat README_02.md ; echo " " ; echo "\pagebreak" ; echo " "; \
-} >> README.md
+  cat generated_chapter_0_en.md ; echo " " ; echo "\pagebreak" ; echo " "; \
+  cat generated_chapter_1_en.md ; echo " " ; echo "\pagebreak" ; echo " "; \
+  cat generated_chapter_2_en.md ; echo " " ; echo "\pagebreak" ; echo " "; \
+} >> generated_book_en.md
+
+{ \
+  cat generated_chapter_0_sv.md ; echo " " ; echo "\pagebreak" ; echo " "; \
+  cat generated_chapter_1_sv.md ; echo " " ; echo "\pagebreak" ; echo " "; \
+  cat generated_chapter_2_sv.md ; echo " " ; echo "\pagebreak" ; echo " "; \
+} >> generated_book_sv.md
 
 # Table of Content
 # Code has highlights following the tango color scheme
 # Thinner margin of 0.5 inch
 # Do not cut code blocks
-pandoc README.md -o book.pdf --toc --toc-depth=1 --highlight-style=book_style.theme -V geometry:margin=0.5in
+pandoc generated_book_en.md -o book_en.pdf --toc --toc-depth=1 --highlight-style=book_style.theme -V geometry:margin=0.5in
+pandoc generated_book_sv.md -o book_sv.pdf --toc --toc-depth=1 --highlight-style=book_style.theme -V geometry:margin=0.5in
 
-cp book.pdf ../../books/book_1_no_front_page.pdf
+cp book_en.pdf ../../books/book_1_no_front_page_en.pdf
+cp book_sv.pdf ../../books/book_1_no_front_page_sv.pdf
 
 cd ../../books || exit 42
-pdfunite front_page_1.pdf book_1_no_front_page.pdf book_1.pdf
+
+pdfunite front_page_1_en.pdf book_1_no_front_page_en.pdf book_1_en.pdf
+pdfunite front_page_1_sv.pdf book_1_no_front_page_sv.pdf book_1_sv.pdf
 
 # Make booklet
-bookletimposer -a book_1.pdf -o booklet_1.pdf
+bookletimposer -a book_1_en.pdf -o booklet_1_en.pdf
+bookletimposer -a book_1_sv.pdf -o booklet_1_sv.pdf
 
 # Cleanup
-rm book_1_no_front_page.pdf
+rm book_1_no_front_page_en.pdf
+rm book_1_no_front_page_sv.pdf
